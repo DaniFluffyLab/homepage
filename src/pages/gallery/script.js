eaf4a6f6706936b673500e9321a288eef8a73f3b
DaniFluffyCat <danifluffycat@outlook.com>
2025-01-25 12:09:46 -0400

    Corrige bugs de tamanhos não-absolutos
    A partir do Javascript, obtém-se o tamanho ideal em valores absolutos para determinada imagem, a fim de evitar quebras de linha indevidas por conta da animação de expansão da imagem

--- src/pages/gallery/script.js
+++ src/pages/gallery/script.js
@@ -1,3 +1,8 @@
+const TRANSICAO_TEMPO = 300
+const IMAGEM_ALTURA = 350
+const IMAGEM_ALTURA_HOVER = 20
+
+/** Coloca ou tira imagem da tela cheia */
 function fullscreen(htmlElement) {
 
     // Verifica se elemento que chamou a função está em tela cheia
@@ -5,22 +10,35 @@ function fullscreen(htmlElement) {
 
     // Seleciona todos os elementos em tela cheia
     document.querySelectorAll(".isFullscreen").forEach(e => {
-        e.style.opacity = "0%";             // Torna-os transparentes
-        setTimeout(() => e.remove(), 300)   // Remove-os em 300ms
+        e.style.opacity = "0%";                         // Torna-os transparentes
+        setTimeout(() => e.remove(), TRANSICAO_TEMPO)   // Remove-os em 300ms
     })
 
     // Se elemento não está em tela cheia
     if (!isFullscreen) {
-        let fsElement = htmlElement.cloneNode(true)         // Clonar elemento
-        fsElement.classList.add("isFullscreen")             // Adicionar classe de tela cheia
-        htmlElement.parentElement.append(fsElement)         // Inserir no corpo do documento
-        setTimeout(() => fsElement.style.opacity = "100%")  // Torna-os opacos
+
+        let fsElement = htmlElement.cloneNode(true)     // Clona elemento
+        fsElement.classList.add("isFullscreen")         // Adiciona classe de tela cheia
+        fsElement.setAttribute("style", "")
+
+        let close = document.createElement("div")   // Cria botão de fechar
+        close.classList.add("close")                // Adiciona classe de fechar ao botão
+        close.textContent = "×"                     // Adiciona X ao botão de fechar
+
+        close.addEventListener("click", () => fullscreen(fsElement))    // Adiciona evento para fechar janela
+        fsElement.append(close)                                         // Insere botão de fechar no elemento
+        htmlElement.parentElement.append(fsElement)                     // Inserir elemento no corpo do documento
+        setTimeout(() => fsElement.style.opacity = "100%")              // Torna-os opacos
     }
 
 }
+// Configura elementos de containeres
+window.addEventListener("load", () => {                             // Assim que janela carregar:
+    document.querySelectorAll(".content main>p").forEach( p => {    // Para cada container:
 
-window.addEventListener("load", () => {
-    document.querySelectorAll(".content main>p").forEach( p => {
-        p.setAttribute("onclick", `fullscreen(this)`)
+        let image = p.querySelector("img")                                              // Encontra imagem no container
+        if (image != null) p.style.width = `${image.width + IMAGEM_ALTURA_HOVER}px`     // Define largura fixa do container
+
+        p.addEventListener("click", () => fullscreen(p))    // Cria evento para colocar imgs em tela cheia
     })
 })
